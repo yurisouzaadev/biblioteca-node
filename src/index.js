b738f6a81c943c41c5d5c3970cb87a2da41ec23c
Yuri Souza <yurisoouza123@gmail.com>
2026-07-10 16:30:37 -0400

    contando as ocorrências de palavras do texto

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) =>{
-    console.log(texto); 
+    verificaPalavrasDuplicadas(texto);
 })
 
+//criar um array com as palavras
+// contar as ocorrências
+// montar um objeto com o resultado
+
+// exemplo: { 'web': 3, 'computador': 5, 'teste': 1 }
+
+
+function verificaPalavrasDuplicadas(texto) {
+    const listaPalavras = texto.split(' ');
+    const resultado = {};
+   // objeto[propriedade] = valor;
+   listaPalavras.forEach(palavra => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1;
+   })
+   console.log(resultado);
+}
