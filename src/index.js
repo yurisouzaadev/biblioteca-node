ef329d0abf0455405807bc6d7b814d4365c4eb1e
Yuri Souza <yurisoouza123@gmail.com>
2026-07-12 21:10:06 -0400

    Utilizando expressão regular para verificar a contagem de caracteres especiais - com o objetivo de contar apenas palavras

--- src/index.js
+++ src/index.js
@@ -23,12 +23,21 @@ function quebrarEmParagrafos(texto) {
 
 }
 
+function limpaPalavras(palavra) {
+    return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
+
+
 function verificaPalavrasDuplicadas(texto) {
     const listaPalavras = texto.split(' ');
     const resultado = {};
    // objeto[propriedade] = valor;
    listaPalavras.forEach(palavra => {
-    resultado[palavra] = (resultado[palavra] || 0) + 1;
+    if (palavra.length >=3){
+    const palavraLimpa = limpaPalavras(palavra);
+    resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1;
+    }
+  
    })
    return resultado;
 }
